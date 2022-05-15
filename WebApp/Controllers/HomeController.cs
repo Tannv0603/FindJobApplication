c98d12c4acfa79376309a1c160aa8511da4ac429
Duy Johannes <bizz432000@gmail.com>
2022-05-15 23:14:21 +0700

    update template

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -29,7 +29,22 @@ namespace WebApp.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult JobDetails()
+        {
+            return View();
+        }
+        public IActionResult JobListing()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
